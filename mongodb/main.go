698e76bb57a76221295905090ff775a562bd99d9
Aly Ragab <AlyRagab70@gmail.com>
2019-07-15 03:52:24 +0200

    Update main.go

--- mongodb/main.go
+++ mongodb/main.go
@@ -12,46 +12,84 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"gopkg.in/mgo.v2/bson"
 )
 
-type User struct {
+type Person struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	FirstName string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
 	LastName  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
 }
 
-var ctx, _ = context.WithTimeout(context.Background(), 60*time.Second)
+var ctx, _ = context.WithTimeout(context.Background(), 20*time.Second)
 var mongoClient *mongo.Client
 var err error
 
-func createUser(response http.ResponseWriter, request *http.Request) {
-	// The Response Type
-	response.Header().Add("content-type", "application/json")
-	var user User
-	json.NewDecoder(request.Body).Decode(&user)
-	db := mongoClient.Database("userdb").Collection("user")
-	result, err := db.InsertOne(ctx, db)
-	check(err)
-	json.NewEncoder(response).Encode(result)
-
-}
-
+// Handling the Error function
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// Create Users in the people Collection
+func createUsers(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Add("content-type", "application/json")
+	var person Person
+	json.NewDecoder(r.Body).Decode(&person)
+	// Create the Database
+	db := mongoClient.Database("usersdb").Collection("people")
+	resultdb, err := db.InsertOne(ctx, db)
+	check(err)
+	// Encode the Json data
+	json.NewEncoder(w).Encode(resultdb)
+
+}
+
+// Delete The Users in the people Collection
+func deleteUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+
+}
+
+// Querying and Getting The users
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+	var people []Person
+	db := mongoClient.Database("usersdb").Collection("people")
+	get, err := db.Find(ctx, bson.M{})
+	check(err)
+	defer get.Close(ctx)
+
+	for get.Next(ctx) {
+		var person Person
+		get.Decode(&person)
+		people = append(people, person)
+
+	}
+	json.NewEncoder(w).Encode(people)
+}
+
+// Insert Users in the people Collection
+func insertUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("content-type", "application/json")
+
+}
+
 func main() {
 
-	//Connection to MongoDB
+	// Database Connection
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
 	mongoClient, err = mongo.Connect(ctx, clientOptions)
 	check(err)
 	fmt.Println("Database Connected Successfully !!")
 
+	//Setting up the Router
 	myRouter := mux.NewRouter()
-	myRouter.HandleFunc("/user", createUser).Methods("POST")
+	myRouter.HandleFunc("/user", createUsers).Methods("POST")
+	myRouter.HandleFunc("/user", deleteUsers).Methods("DELETE")
+	myRouter.HandleFunc("/user", getUsers).Methods("GET")
+	myRouter.HandleFunc("/user", insertUsers).Methods("PUT")
 	err = http.ListenAndServe(":7070", myRouter)
 }
-
